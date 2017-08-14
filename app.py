49cd4423850d4eec9667172c6237eb3240596f96
Micheál Looney <looneymicheal@gmail.com>
2017-08-14 16:49:51 +0100

    Fix whitespace

--- app.py
+++ app.py
@@ -3,20 +3,15 @@ import hmac
 
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 ID_VERIFICATION_SECRET = ""
 APP_ID = ""
-
-
 
 def get_user_hash(user):
     data = user['email']
     result = hmac.new(ID_VERIFICATION_SECRET, data, hashlib.sha256).hexdigest()
     return result
     
-
-
 @app.route('/')
 def index():
     user = {}
@@ -32,6 +27,6 @@ def index():
 
     return render_template('index.html', user=user, app_id=APP_ID)     
 
-
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)  
+    
